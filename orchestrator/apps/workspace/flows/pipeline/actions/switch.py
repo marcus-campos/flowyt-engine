f96916b254d23ad03037bbbd364bd77787314a98
Marcus Vinicius Campos <marcus_vinicius_campos@hotmail.com>
2019-12-26 17:52:48 -0300

    chore(lint): fix lint warnings

--- orchestrator/apps/workspace/flows/pipeline/actions/switch.py
+++ orchestrator/apps/workspace/flows/pipeline/actions/switch.py
@@ -4,10 +4,9 @@ from utils.action import GenericAction
 
 
 class Switch(GenericAction):
-
     def start(self, context):
         self.action_data = self.load_action_data(self.action_data, context)
-        
+
         next_action = self.handle(self.action_data, context)
         pipeline_context = {"next_action": next_action}
 
@@ -16,30 +15,38 @@ class Switch(GenericAction):
     def handle(self, action_data, context):
         next_action = None
 
-        for condition in action_data.get('conditions'):
-            if condition.get('operator') == 'equal':
-                result = condition.get('first_expression') == condition.get('second_expression')
-                next_action = condition.get('next_action') if result else None
+        for condition in action_data.get("conditions"):
+            if condition.get("operator") == "equal":
+                result = condition.get("first_expression") == condition.get(
+                    "second_expression"
+                )
+                next_action = condition.get("next_action") if result else None
                 break
 
-            if condition.get('operator') == 'different':
-                result = condition.get('first_expression') != condition.get('second_expression')
-                next_action = condition.get('next_action') if result else None
+            if condition.get("operator") == "different":
+                result = condition.get("first_expression") != condition.get(
+                    "second_expression"
+                )
+                next_action = condition.get("next_action") if result else None
                 break
 
-            if condition.get('operator') == 'greater than':
-                result = condition.get('first_expression') > condition.get('second_expression')
-                next_action = condition.get('next_action') if result else None
+            if condition.get("operator") == "greater than":
+                result = condition.get("first_expression") > condition.get(
+                    "second_expression"
+                )
+                next_action = condition.get("next_action") if result else None
                 break
 
-            if condition.get('operator') == 'less than':
-                result = condition.get('first_expression') < condition.get('second_expression')
-                next_action = condition.get('next_action') if result else None
+            if condition.get("operator") == "less than":
+                result = condition.get("first_expression") < condition.get(
+                    "second_expression"
+                )
+                next_action = condition.get("next_action") if result else None
                 break
 
         if not next_action:
-            condition_else = action_data.get('conditions')[-1]
-            if condition_else.get('operator') == 'else':
-                next_action = condition_else.get('next_action')
-     
+            condition_else = action_data.get("conditions")[-1]
+            if condition_else.get("operator") == "else":
+                next_action = condition_else.get("next_action")
+
         return next_action
