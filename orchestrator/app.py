abffa520b17b01b4dc396fa8c5280f042203a6fc
Will <william@imotion-info.com>
2020-01-02 12:42:45 -0300

    Revert "chore(config): load configs from settings.py"
    This reverts commit 14f000d9ff0b71c7b60f05c28f163c2e5d8621ad.

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_restful import Api
 
 from orchestrator.urls import load_urls
+from orchestrator.settings import SERVER_NAME
 
 app = Flask(__name__)
 
-# App config
-app.config.from_object("orchestrator.settings")
-
+# App configs
+app.config["SERVER_NAME"] = SERVER_NAME
 
 api = Api(app)
 # Load urls
 api = load_urls(api)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- orchestrator/orchestrator/settings.py
+++ orchestrator/orchestrator/settings.py
@@ -1,11 +1,8 @@
 import os
 
 # System
-SECRET_KEY="de5ee9689580bed0175b2b79053799d30b7a1f09c7d8f483"
-ENV='development' #'production'
-DEBUG=True
-SERVER_NAME="orchestrator.local:5000"
+BASE_DIR = os.getcwd()
+SERVER_NAME = "orchestrator.local:5000"
 
 # Apps
-BASE_DIR=os.getcwd()
-WORKSPACES_DIR="/workspaces"
+WORKSPACES_DIR = "/workspaces"
