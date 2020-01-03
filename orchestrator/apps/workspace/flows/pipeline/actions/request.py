509ff44009dc2947b891353b95391c3c143c00f4
Will <william@imotion-info.com>
2020-01-03 12:43:16 -0300

    Correct identation

--- orchestrator/apps/workspace/flows/pipeline/actions/request.py
+++ orchestrator/apps/workspace/flows/pipeline/actions/request.py
@@ -1,10 +1,14 @@
-import re
-
 from utils.action import GenericAction
 from utils.http import HttpRequest
 
+HTTP_STATUS_OK_200 = 200
+HTTP_STATUS_MULTIPLE_CHOICES_300 = 300
+
 
 class Request(GenericAction):
+    def __init__(self):
+        self.action_data = None
+
     def start(self, context):
         self.action_data = self.load_action_data(self.action_data, context)
         context = self.handle(self.action_data, context)
@@ -12,7 +16,7 @@ class Request(GenericAction):
         status_code = context.public.response.get("status_code")
         pipeline_context = {}
 
-        if status_code < 200 or status_code > 299:
+        if status_code < HTTP_STATUS_OK_200 or status_code >= HTTP_STATUS_MULTIPLE_CHOICES_300:
             pipeline_context["next_action"] = self.action_data.get("next_action_fail")
         else:
             pipeline_context["next_action"] = self.action_data.get("next_action_success")
@@ -27,7 +31,7 @@ class Request(GenericAction):
         }
 
         handler = getattr(self, request.method.lower())
-        response = handler(request_data)
+        response = handler(request, request_data)
 
         context.public.response = {
             "status_code": response.status_code,
@@ -38,19 +42,19 @@ class Request(GenericAction):
 
         return context
 
-        def get(self, request_data):
-            request_data["params"] = request_data["data"]
-            del request_data["data"]
-            return request.get(**request_data)
+    def get(self, request, request_data):
+        request_data["params"] = request_data["data"]
+        del request_data["data"]
+        return request.get(**request_data)
 
-        def post(self, request_data):
-            return request.post(**request_data)
+    def post(self, request, request_data):
+        return request.post(**request_data)
 
-        def patch(self, request_data):
-            return request.put(**request_data)
+    def patch(self, request, request_data):
+        return request.put(**request_data)
 
-        def put(self, request_data):
-            return request.patch(**request_data)
+    def put(self, request, request_data):
+        return request.patch(**request_data)
 
-        def delete(self, request_data):
-            return request.delete(**request_data)
+    def delete(self, request, request_data):
+        return request.delete(**request_data)
