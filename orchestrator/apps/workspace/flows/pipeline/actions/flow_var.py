ee6a15d2e0c5a1a575a996403fd82f38e7fbb860
Marcus Vinicius Campos <marcus_vinicius_campos@hotmail.com>
2020-01-07 10:38:01 -0300

    refactor(action): edit the action base

--- orchestrator/apps/workspace/flows/pipeline/actions/flow_var.py
+++ orchestrator/apps/workspace/flows/pipeline/actions/flow_var.py
@@ -4,4 +4,4 @@ from utils.action import GenericAction
 class FlowVar(GenericAction):
     def handle(self, action_data, context):
         context.public.flow = {**context.public.flow, **action_data}
-        return context
+        return context, None
--- orchestrator/apps/workspace/flows/pipeline/actions/response.py
+++ orchestrator/apps/workspace/flows/pipeline/actions/response.py
@@ -1,31 +1,8 @@
 import re
-import ast
 from utils.action import GenericAction
 
 
 class Response(GenericAction):
-    def start(self, context):
-        self.action_data = self.load_action_data(self.action_data, context)
-        context = self.handle(self.action_data, context)
-        self.action_data = self.__load_dict_fields(self.action_data)
+    def handle(self, action_data, context):
         pipeline_context = {"response": self.action_data}
-        return self.next_action(context, pipeline_context)
-
-    def __load_dict_fields(self, action_data):
-        # Load json fields
-        for key in action_data:
-            if isinstance(action_data[key], dict):
-                self.__load_dict_fields(action_data[key])
-
-            data = self.__is_dict(str(action_data[key]))
-
-            if data:
-                action_data[key] = data
-
-        return action_data
-
-    def __is_dict(self, data):
-        try:
-            return ast.literal_eval(data)
-        except:
-            return False
+        return context, pipeline_context
