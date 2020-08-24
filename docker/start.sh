3ef619a10f071985ff71d739b2d4f3bd748ea2bf
Ulysses Monteiro <ulysses.monteiro@ish.com.br>
2020-08-24 21:24:44 +0000

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-service cron start
 
-croncmd="/usr/local/bin/python /usr/src/app/flowyt/cli.py -w servicenow -f contracts -d true > /tmp/debug.log 2>&1"
-cronjob="0 0 * * * $croncmd"
-( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+# linha cron para artisan schedule
+/bin/echo "0 0 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w servicenow -f contracts -d true > /tmp/debug.log 2>&1" >/tmp/jobs.cron
+
+#inicia cron e adiciona script scheduler laravel
+cron
+crontab /tmp/jobs.cron
+
+# .env Build
+/bin/bash ./docker/varsenv.sh
+
+service cron start
 
 bash
