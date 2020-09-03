17ac4389e2e81737eb962780e78c0200cf999472
Ulysses Monteiro <ulysses.monteiro@ish.com.br>
2020-09-03 12:50:50 +0000

    Update start.sh, fixed

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,9 @@
 
 
 # linha cron para artisan schedule
-/bin/echo "0 0 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w servicenow -f chain -d true > /tmp/debug.log 2>&1" >/tmp/jobs.cron
-/bin/echo "0 30 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w sapi -f vulnerabilities -d true > /tmp/debug.log 2>&1" >/tmp/jobs.cron
-/bin/echo "0 1 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w massification -f chain -d true > /tmp/debug.log 2>&1" >/tmp/jobs.cron
+/bin/echo "0 0 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w servicenow -f chain -d true >> /dev/stdout 2>&1" >/tmp/jobs.cron
+/bin/echo "0 30 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w sapi -f vulnerabilities -d true >> /dev/stdout 2>&1" >>/tmp/jobs.cron
+/bin/echo "0 1 * * * /usr/local/bin/python /usr/src/app/flowyt/cli.py -w massification -f chain -d true >> /dev/stdout 2>&1" >>/tmp/jobs.cron
 
 #inicia cron e adiciona script scheduler laravel
 cron
